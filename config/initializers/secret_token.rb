1ba23145c4481ba20159856792c8410176775786
lyfeyaj <liuyuqqq123@gmail.com>
2013-07-31 20:09:37 +0800

    create zhaopin-test app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZhaopinTest::Application.config.secret_key_base = 'c37a003a07dca9c2d386efada94220b59530609056e5cf7455fcbc548112b0278a90b78cc10c1929d34abed556363641e1941775eb7e1ed26a9d2a75c0ee5016'
